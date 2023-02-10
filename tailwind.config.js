fdeb75fe83e8ed9d026b8edb151ee5928f1276a7
Babinson Batala <bobbybatala320@gmail.com>
2023-02-09 22:47:21 -0500

    made edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
@@ -14,4 +15,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
